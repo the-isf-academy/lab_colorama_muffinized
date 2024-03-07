abeca4b28fde7c7dd1219c126789ed4c31dfcf64
Emma <eqbrown@isf.edu.hk>
2024-03-07 11:22:23 +0800

    update views files for 2023-24

--- color_app/urls.py
+++ color_app/urls.py
@@ -5,6 +5,6 @@ from color_app import views
 app_name = "color_app"
 urlpatterns = [
     path('', views.home_view, name="index"),
-    path('random/', views.random_color_view, name="random_color"),
+    path('random/', views.random_color_view, name="random_color")
 ]
 
--- color_app/views.py
+++ color_app/views.py
@@ -1,30 +1,54 @@
 from django.shortcuts import render
-import logging
 from color_app.models import Color
 from random import randint
+from django.views.generic import DetailView, ListView, CreateView
+from django.urls import reverse_lazy
+from color_app.models import Color
+from color_app.forms import ColorForm
 
-log = logging.getLogger(__name__)
+#######################
+# Function Based Views
+#######################
+
 
 def home_view(request):
     "A view function which renders the homepage"
-    log.info("In color_app.views.home. Rendering the homepage.")
+
     skyblue = Color(name="skyblue", red=135, green=206, blue=250)
+
     params = {
         "name": "stranger",
         "color": skyblue,
     }
+    
     response = render(request, 'color_app/index.html', params)
     return response
 
 def random_color_view(request):
     "A view function which renders a random color"
-    log.info("In color_app.views.home. Rendering a random color page.")
+
     random_color = Color(
         name="random color", 
         red=randint(0, 256), 
         green=randint(0, 256),
         blue=randint(0, 256)
     )
+
     params = {"color": random_color}
+
     return render(request, 'color_app/random_color.html', params)
 
+#######################
+# Class Based Views
+#######################
+
+class ColorListView(ListView):
+    model = Color
+    template_name = "color_app/color_list.html"
+    queryset = Color.objects.order_by("name")
+
+class NewColorView(CreateView):
+    model = Color
+    form_class = ColorForm
+    template_name = "color_app/color_form.html"
+    success_url = reverse_lazy("color_app:color_list")
